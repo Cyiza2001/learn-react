75ab12a34ec39f0ddec4d453a277689c2e53dd22
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-06-30 11:04:52 +0200

    another commit

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const name = "manyembua";
+  const isNameShowing = false;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ndahitumye sha nubw nkiri {isNameShowing ? name : "Djibuji"}</h1>
     </div>
   );
-}
+};
 
 export default App;
