111b9b94b8ad95b40ffc8ce5a41f89cff813f359
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-07-04 21:52:31 +0200

    another commit

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
 
 //56a8f03b
 const API_URL = "http://www.omdbapi.com?apikey=56a8f03b";
+const movie1 = {
+  Title: "Amazing Spiderman Syndrome",
+  Year: "2012",
+  imdbID: "tt2586634",
+  Type: "movie",
+  Poster: "N/A",
+};
 
 const App = () => {
   const searchMovies = async (title) => {
@@ -15,7 +24,29 @@ const App = () => {
     searchMovies("spiderman");
   }, []);
 
-  return <h1>APP</h1>;
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="search for movies"
+          value="superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img src={movie1.Poster} alt={movie1.Title} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
